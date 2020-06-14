db5f58011780a3327ca4af7da9ffa652ec04aad5
MeetPanchani <mmeetpanchani63@gmail.com>
2020-06-14 19:58:47 +0530

    Updates

---
+++ precache-manifest.7f85975bd191b529a7076cc677a5e7a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6434275575324c7035e3344f88723c",
+    "url": "/phone-store/index.html"
+  },
+  {
+    "revision": "9e5d681748399816997e",
+    "url": "/phone-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "03d025c5423df6aec9c1",
+    "url": "/phone-store/static/css/main.cfa72b9c.chunk.css"
+  },
+  {
+    "revision": "9e5d681748399816997e",
+    "url": "/phone-store/static/js/2.d58becfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phone-store/static/js/2.d58becfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d025c5423df6aec9c1",
+    "url": "/phone-store/static/js/main.14ab22eb.chunk.js"
+  },
+  {
+    "revision": "1b7649f34bcdee189ff0",
+    "url": "/phone-store/static/js/runtime-main.0a0edbe6.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/phone-store/static/media/logo.9f7e506d.svg"
+  }
+]);
